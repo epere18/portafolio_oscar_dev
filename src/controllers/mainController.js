f1e744e98eecfc42744547ddb161e87726dff68f
epere18 <epere18@hotmail.com>
2024-09-12 18:26:21 -0300

    Subiendo main.css

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,7 +1,17 @@
 const path = require("path");
 const fs = require("fs");
+const { title } = require("process");
+
+const mainController = {
+
+    index : (req, res) => {
+        res.render("index",{title: "Home"});
+    }
 
 
+
+
+}
 
 
 
